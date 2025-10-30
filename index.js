48f140ee9e3b402a744517c4896cd8e49ed94bf7
Abd0id <abdallahidfat@gmail.com>
2025-10-30 16:39:25 +0100

    fixing questions array and making a welcome screen

--- index.js
+++ index.js
@@ -1,36 +1,70 @@
-const questions = [
-  "Why did the chicken cross the road?",
-  "what are you doing bruh?",
-  "why did you redeem it?",
-  "who did it?",
-  "do you still have time?",
-];
-
-
-
 const nextButton = document.getElementById("nextBtn");
 const prevButton = document.getElementById("prevBtn");
+const myChoices = document.getElementById("choices");
 
-var i = 0;
+let questions = [
+  {
+    question: "Hi?",
+  },
+  {
+    question: "Why did the chicken cross the road?",
+    answer: "beqauuuuse",
+  },
+  {
+    question: "what are you doing bro?",
+    answer: "idk",
+  },
+  {
+    question: "why did you redeem it?",
+    answer: "nooooooo",
+  },
+  {
+    question: "what's 9 + 10?",
+    answer: "21?",
+  },
+  {
+    question: "do you still have time?",
+    answer: "NaN",
+  },
+];
+
+let myQuestion = questions[0].question;
+let myAnswer = questions[0].answer;
+
+console.log(myQuestion);
+console.log(myAnswer);
+welcomeScreen();
 nextButton.addEventListener("click", () => {
   switchQuestion(true);
 });
+
 prevButton.addEventListener("click", () => {
   switchQuestion(false);
 });
 
+prevButton.style.display = "none";
+
+function welcomeScreen() {
+  document.getElementById("head-text").textContent = "Welcome";
+  myChoices.style.display = "none";
+  prevButton.style.display = "none";
+  i = 0;
+}
+
 function switchQuestion(isNext) {
-  console.log("hi");
-  if (isNext && i < 5) {
-    document.getElementById("question").textContent = questions[i];
-    document.getElementById("head-text").textContent = "Quiz";
+  if (isNext && i <= 5) {
     i++;
-  } else {
+    document.getElementById("question").textContent = questions[i].question;
+    document.getElementById("head-text").textContent = "Quiz";
+    myChoices.style.display = "";
+
+    prevButton.style.display = "";
+  } else if (!isNext) {
     --i;
-    document.getElementById("question").textContent = questions[i];
-    if (i < 0) {
-      document.getElementById("head-text").textContent = "Welcome";
-      i = 0;
+    document.getElementById("question").textContent = questions[i].question;
+    if (i <= 0) {
+      welcomeScreen();
     }
   }
+  console.log(i);
 }
